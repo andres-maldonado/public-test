4d7d1af31c61ec885ecbf275bce3fa562a813ae2
Andrés Maldonado <contact.a.maldonado@gmail.com>
2019-11-07 19:28:22 -0300

    Update potito.py

--- potito.py
+++ potito.py
@@ -1,1 +1,6 @@
-print("Me gustaria ser mojon para atraversarle el hoyo")
+def poema(nivel):
+  if nivel == 'poeta':
+     print("Me gustaria ser mojon para atraversarle el hoyo")
+   
+  elif nivel == 'mamón':
+     print('Es bacán')
