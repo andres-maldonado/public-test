efeb93b430a82f94eb02f6d13a2ebe58881e1b3c
felipe0216 <felipe.araya.1993@gmail.com>
2019-11-07 22:46:34 +0000

    update
    lo que hice fue chulapi

--- potito.py
+++ potito.py
@@ -1,6 +1,10 @@
-def poema(nivel):
+﻿def poema(nivel):
   if nivel == 'poeta':
      print("Me gustaria ser mojon para atraversarle el hoyo")
    
   elif nivel == 'mamón':
      print('Es bacán')
+
+
+
+print("chulapi")
